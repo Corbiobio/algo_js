64368646c6f8ef4d6fc47b3df9cdfd3843dcb491
corbiobio <edarnand@edenschool.fr>
2023-04-27 16:37:03 +0200

    multiple 3 5 done

--- algo_code_wars/narcissitic_num.js
+++ algo_code_wars/narcissitic_num.js
@@ -1,13 +1,13 @@
-//  https://www.codewars.com/kata/5287e858c6b5a9678200083c/train/javascript
+// //  https://www.codewars.com/kata/5287e858c6b5a9678200083c/train/javascript
 
 
-function narcissitic(num) {
-    let arr_num = num.toString().split('').map(Number);
-    let total = 0
-    for (let i = 0; i < arr_num.length; i++) {
-        total += Math.pow(arr_num[i], arr_num.length)
-    }
-    return num === total ? true : false
-}
+// function narcissitic(num) {
+//     let arr_num = num.toString().split('').map(Number);
+//     let total = 0
+//     for (let i = 0; i < arr_num.length; i++) {
+//         total += Math.pow(arr_num[i], arr_num.length)
+//     }
+//     return num === total ? true : false
+// }
 
-console.log(narcissitic(153));
+// console.log(narcissitic(153));
