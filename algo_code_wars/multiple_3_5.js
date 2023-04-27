096973991d2820f22c0b49b2a9a5d26ebac3db26
corbiobio <edarnand@edenschool.fr>
2023-04-27 15:41:16 +0200

    narcissitic done

---
+++ algo_code_wars/multiple_3_5.js
@@ -0,0 +1,8 @@
+// // https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
+
+// function multiple(num) {
+//     if (num % 3 === 0 && num % 5 === 0) {
+//         console.log(true);
+//     }
+// }
+// console.log(multiple(11));
