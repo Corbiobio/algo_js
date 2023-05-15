d3517d937a7012aaaf5f8a1f309f2de7a02c17da
corbiobio <edarnand@edenschool.fr>
2023-05-15 16:53:49 +0200

    algo douche froide done

--- algo_code_wars/multiple_3_5.js
+++ algo_code_wars/multiple_3_5.js
@@ -1,25 +1,25 @@
-// https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
+// // https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
 
-function multiple(num) {
+// function multiple(num) {
 
-    // total_3 += Math.floor(num / 3) * 3
-    // console.log(total);
+//     // total_3 += Math.floor(num / 3) * 3
+//     // console.log(total);
 
-    // total += Math.floor(num / 5) * 5
-    // console.log(total);
-    let total = 0
-    for (let total_3 = 0; total_3 < num; total_3 += 3) {
-        total += total_3
-    }
-    for (let total_5 = 0; total_5 < num; total_5 += 5) {
-        total += total_5
-    }
-    for (let total_15 = 0; total_15 < num; total_15 += 15) {
-        // console.log(total_15);
-        total -= total_15
-    }
-    console.log(total);
-    return total
-}
+//     // total += Math.floor(num / 5) * 5
+//     // console.log(total);
+//     let total = 0
+//     for (let total_3 = 0; total_3 < num; total_3 += 3) {
+//         total += total_3
+//     }
+//     for (let total_5 = 0; total_5 < num; total_5 += 5) {
+//         total += total_5
+//     }
+//     for (let total_15 = 0; total_15 < num; total_15 += 15) {
+//         // console.log(total_15);
+//         total -= total_15
+//     }
+//     console.log(total);
+//     return total
+// }
 
-console.log(multiple(20));
+// console.log(multiple(20));
