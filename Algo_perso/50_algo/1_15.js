133ac65320f3799499b5733df26cdb53ae2493fc
corbiobio <edarnand@edenschool.fr>
2023-06-12 09:54:35 +0200

    50 exo js add, algo 1 a 15 done

---
+++ Algo_perso/50_algo/1_15.js
@@ -0,0 +1,118 @@
+//1
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// 2
+// for (let i = 1; i <= 100; i += 2) {
+//     console.log(i);
+// }
+
+//3
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i * 7);
+// }
+
+//4
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`table of ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(i * j);
+//     }
+// }
+
+//5
+// let total = 0
+// for (let i = 0; i <= 10; i++) {
+//     console.log(total += i);
+// }
+
+// 6
+// let total = 1
+// for (let i = 1; i <= 10; i++) {
+//     total = total * i
+//     console.log(total);
+// }
+
+
+// 7
+// let total = 0
+// for (let i = 10; i <= 30; i += 2) {
+//     total += i
+//     console.log(total);
+// }
+
+//8
+// let celsius = 504
+// console.log(32 + (celsius * 1.8));
+
+//9
+// let fahrenheit = 500
+// console.log((fahrenheit - 32) * 5 / 9);
+
+//10
+// let array = [50, 42, 2, 9, 4, 3, 7, 575, 5, 645, 42]
+// let total = 0
+// for (let i = 0; i < array.length; i++) {
+//     total += array[i]
+// }
+// console.log(total);
+
+// 11
+// let num = [50, 10, 2, 4]
+// let total = 0
+// for (let i = 0; i < num.length; i++) {
+//     total += num[i]
+// }
+// console.log(total / num.length);
+
+//12
+// function positive_num(arr_num) {
+//     let positive_num = []
+//     for (let i = 0; i < arr_num.length; i++) {
+//         if (arr_num[i] > 0) {
+//             positive_num.push(arr_num[i])
+//         }
+//     }
+//     console.log(positive_num);
+// }
+// let num = [50, -5, -2, 75, -0, -6054]
+// positive_num(num)
+
+//13
+// let num = [50, , 860 - 5, 7800, -2, 75, -0, -6054]
+// let max = 0
+// for (let i = 0; i < num.length; i++) {
+//     if (num[i] > max) {
+//         max = num[i]
+//     }
+// }
+// console.log(max);
+
+//14
+// let fibonacci = 1
+// let last_fibonacci = 0
+
+// for (let i = 1; i < 10; i++) {
+//     let temp = fibonacci
+//     fibonacci += last_fibonacci
+
+//     last_fibonacci = temp
+//     console.log(fibonacci);
+// }
+
+//15
+// function fibonacci(num_to_reach) {
+//     let fibonacci = 1
+//     let last_fibonacci = 0
+
+//     for (let i = 1; i < num_to_reach; i++) {
+//         let temp = fibonacci
+//         fibonacci += last_fibonacci
+
+//         last_fibonacci = temp
+//     }
+//     console.log(fibonacci);
+// }
+// fibonacci(8)
--- tainix/[2]coach_de_foot.js
+++ tainix/[2]coach_de_foot.js
@@ -1,85 +1,85 @@
-const dispositif = '3-4-3';
-const forces = [21, 9, 10, 59, 47, 41, 28, 30, 15, 14, 45, 6, 33, 54, 49, 4, 2, 20, 35, 25, 46, 40, 51, 44, 31, 24, 29, 58, 7, 12, 34];
-const postes = ['G', 'M', 'A', 'A', 'D', 'M', 'A', 'A', 'M', 'M', 'M', 'D', 'M', 'M', 'D', 'D', 'A', 'M', 'A', 'A', 'D', 'M', 'M', 'A', 'M', 'A', 'A', 'A', 'A', 'A', 'D'];
+// const dispositif = '3-4-3';
+// const forces = [21, 9, 10, 59, 47, 41, 28, 30, 15, 14, 45, 6, 33, 54, 49, 4, 2, 20, 35, 25, 46, 40, 51, 44, 31, 24, 29, 58, 7, 12, 34];
+// const postes = ['G', 'M', 'A', 'A', 'D', 'M', 'A', 'A', 'M', 'M', 'M', 'D', 'M', 'M', 'D', 'D', 'A', 'M', 'A', 'A', 'D', 'M', 'M', 'A', 'M', 'A', 'A', 'A', 'A', 'A', 'D'];
 
-function sort_player(players, power, compo) {
-    let keeper = []
-    let defender = []
-    let midfielder = []
-    let attacker = []
+// function sort_player(players, power, compo) {
+//     let keeper = []
+//     let defender = []
+//     let midfielder = []
+//     let attacker = []
 
 
-    for (let i = 0; i < players.length; i++) {
-        switch (players[i]) {
-            case "G":
-                keeper.push({ power: power[i], index: i })
-                break
-            case "D":
-                defender.push({ power: power[i], index: i })
-                break
-            case "M":
-                midfielder.push({ power: power[i], index: i })
-                break
-            case "A":
-                attacker.push({ power: power[i], index: i })
-                break
-        }
-    }
+//     for (let i = 0; i < players.length; i++) {
+//         switch (players[i]) {
+//             case "G":
+//                 keeper.push({ power: power[i], index: i })
+//                 break
+//             case "D":
+//                 defender.push({ power: power[i], index: i })
+//                 break
+//             case "M":
+//                 midfielder.push({ power: power[i], index: i })
+//                 break
+//             case "A":
+//                 attacker.push({ power: power[i], index: i })
+//                 break
+//         }
+//     }
 
-    function best_player(players) {
-        best = players[0]
-        let index = 0
-        for (let i = 1; i < players.length; i++) {
-            if (players[i].power > best.power) {
-                best = players[i]
-                index = i
-            }
-        }
-        players.splice(index, 1)
-        return { best_player: best, new_players: players }
-    }
+//     function best_player(players) {
+//         best = players[0]
+//         let index = 0
+//         for (let i = 1; i < players.length; i++) {
+//             if (players[i].power > best.power) {
+//                 best = players[i]
+//                 index = i
+//             }
+//         }
+//         players.splice(index, 1)
+//         return { best_player: best, new_players: players }
+//     }
 
-    function list_best_player(num_of_player, players) {
+//     function list_best_player(num_of_player, players) {
 
-        if (num_of_player <= players.length) {
+//         if (num_of_player <= players.length) {
 
-            let player_list = []
-            for (let i = 0; i < num_of_player; i++) {
+//             let player_list = []
+//             for (let i = 0; i < num_of_player; i++) {
 
-                let result = best_player(players)
-                players = result.new_players
-                player_list.push(result.best_player)
-            }
-            return player_list
-        }
-        else {
-            return [false]
-        }
-    }
+//                 let result = best_player(players)
+//                 players = result.new_players
+//                 player_list.push(result.best_player)
+//             }
+//             return player_list
+//         }
+//         else {
+//             return [false]
+//         }
+//     }
 
-    console.log(compo);
+//     console.log(compo);
 
-    let all_best_player = []
-    let enough_player = true
+//     let all_best_player = []
+//     let enough_player = true
 
-    list_best_player(1, keeper).forEach((player) => { if (player) { all_best_player.push(player) } else { enough_player = false } })
-    list_best_player(compo[0], defender).forEach((player) => { if (player) { all_best_player.push(player) } else { enough_player = false } })
-    list_best_player(compo[2], midfielder).forEach((player) => { if (player) { all_best_player.push(player) } else { enough_player = false } })
-    list_best_player(compo[4], attacker).forEach((player) => { if (player) { all_best_player.push(player) } else { enough_player = false } })
+//     list_best_player(1, keeper).forEach((player) => { if (player) { all_best_player.push(player) } else { enough_player = false } })
+//     list_best_player(compo[0], defender).forEach((player) => { if (player) { all_best_player.push(player) } else { enough_player = false } })
+//     list_best_player(compo[2], midfielder).forEach((player) => { if (player) { all_best_player.push(player) } else { enough_player = false } })
+//     list_best_player(compo[4], attacker).forEach((player) => { if (player) { all_best_player.push(player) } else { enough_player = false } })
 
-    console.log(all_best_player);
+//     console.log(all_best_player);
 
-    if (enough_player) {
-        let index_of_best_player = ""
+//     if (enough_player) {
+//         let index_of_best_player = ""
 
-        while (all_best_player.length !== 0) {
-            index_of_best_player += best_player(all_best_player).best_player.index + "-"
-        }
-        index_of_best_player = index_of_best_player.slice(0, -1)
-        console.log(index_of_best_player);
+//         while (all_best_player.length !== 0) {
+//             index_of_best_player += best_player(all_best_player).best_player.index + "-"
+//         }
+//         index_of_best_player = index_of_best_player.slice(0, -1)
+//         console.log(index_of_best_player);
 
-    } else {
-        return "KO"
-    }
-}
-sort_player(postes, forces, dispositif)
+//     } else {
+//         return "KO"
+//     }
+// }
+// sort_player(postes, forces, dispositif)
