2e438d385fa5cd83d47888804889c35aa70f6c51
corbiobio <edarnand@edenschool.fr>
2023-06-20 11:37:38 +0200

    code wars to camel case done

---
+++ algo_code_wars/toCamelCase.js
@@ -0,0 +1,21 @@
+let string = "sdddtr_ytryry_rtyrty-rtyrh-fdgdf_ret"
+
+function toCamelCase(sentence) {
+    let index_replace = []
+    for (let i = 0; i < sentence.length; i++) {
+        if (sentence[i] === "_") {
+            sentence = sentence.replace("_", "")
+            index_replace.push(i)
+        } else if (sentence[i] === "-") {
+            sentence = sentence.replace("-", "")
+            index_replace.push(i)
+        }
+    }
+    sentence = sentence.split("")
+    index_replace.forEach(element => {
+        sentence[element] = sentence[element].toUpperCase()
+    });
+
+    return sentence = sentence.join('')
+}
+console.log(toCamelCase(string));
